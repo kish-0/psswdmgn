da7d3742747d73bbaa0a00d8a6b679d9d257a73a
Kishan <senthilkumarkishan@gmail.com>
2025-03-26 17:55:05 +0530

    started 🚀

---
+++ main.py
@@ -0,0 +1,39 @@
+#Main
+import csv
+import argparse
+
+def main():
+    #write(data)
+    print(find())
+
+def write():
+    service = input("service: ")
+    uname = input("username: ")
+    psswd = input("password: ")
+    d = {'service': service, 'uname': uname, 'psswd': psswd}
+
+    with open('data.csv', newline='') as csvfile: # Check if username and service already exists
+        reader = csv.DictReader(csvfile)
+        for line in reader:
+            if line['service'] == d['service'] and line['uname'] == d['uname']:
+                raise ValueError(f"password for {line['uname']} already exists !")
+
+    with open('data.csv', 'a', newline='') as datafile: # Write to csv file
+        fields = ['service', 'uname', 'psswd']
+        datawriter = csv.DictWriter(datafile, fieldnames=fields)
+        datawriter.writerow(d)
+
+def find():
+    service = input("service: ")
+    usname = input("username: ")
+
+    with open('data.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for line in reader:
+            if line['service'] == service and line['uname'] == usname:
+                return line['psswd']
+        else:
+            return "Service not found !"
+        
+main()
