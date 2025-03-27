e7d0be2f8aacbd2ff6a66c197833d380c8d82126
Kishan <senthilkumarkishan@gmail.com>
2025-03-27 22:04:11 +0530

    psswdmgn package 🚀

--- __main__.py
+++ __main__.py
@@ -7,15 +7,20 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, BASE_DIR)
 from scripts import newpasswd, view
 
-parser = argparse.ArgumentParser(description="Add/Generate and save passwords securely")
+def main():
+    parser = argparse.ArgumentParser(description="Add/Generate and save passwords securely")
 
-action = parser.add_mutually_exclusive_group()
-action.add_argument("-n", "--newpsswd", help="Used to add and save psswd of new service", nargs="?", const="noservicegiven")
-action.add_argument("-v", "--view", help="Used to view saved credentials", nargs="?", const="all")
+    action = parser.add_mutually_exclusive_group()
+    action.add_argument("-n", "--newpsswd", help="Used to add and save psswd of new service", nargs="?", const="noservicegiven")
+    action.add_argument("-v", "--view", help="Used to view saved credentials", nargs="?", const="all")
 
-args = vars(parser.parse_args())
+    args = vars(parser.parse_args())
 
-if args['view']:
-    print(view(args["view"]))
-elif args['newpsswd']:
-    print(newpasswd(args["newpsswd"]))
+    if args['view']:
+        print(view(args["view"]))
+    elif args['newpsswd']:
+        print(newpasswd(args["newpsswd"]))
+
+
+if __name__ == "__main__":
+    main()
